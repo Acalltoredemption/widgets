5721481429dc86ebb74851f3bfdfb8dc5280ed9e
Acalltoredemption <acalltoredemption@gmail.com>
2020-12-01 15:27:40 -0800

    began implementing custom routing into app.js via route.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
-
+import Route from './components/Route';
 
 
 
@@ -37,11 +37,54 @@ const options = [
 },
 ];
 
+const showAccordion = () => {
+    if (window.location.pathname === '/'){
+        return <Accordion items={items} />
+    }
+}
+
+const showList = () => {
+    if (window.location.pathname === '/list'){
+        return <Search />;
+    }
+}
+
+const showDropdown = () => {
+    if (window.location.pathname === '/dropdown'){
+        return <Dropdown />
+    }
+}
+
+const showTranslate = () => {
+    if (window.location.pathname === '/translate'){
+        return <Translate />
+    }
+}
+
+
+
 export default () => {
+const [selected, setSelected] = useState(options[0]);
 
     return (
         <div>
-           <Translate />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown label="Select a Color"
+                options={options}
+                selected={selected}
+                onSelectedChange={setSelected}
+                 />
+            </Route>
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+          
         </div>
     );
 };
